1fb24efeea9f5dd22a392c6096bd08c906f5740e
Evan Gunning <evan.gunning@goat.com>
2023-06-10 10:57:30 -0700

    cleanup cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -5,15 +5,8 @@ program
   .requiredOption(
     "-d, --dir <dir>, Specify the directory where your statements are located"
   )
-  .action((options) => {
-    const dirPath = options.dir;
-
-    console.log(options);
-    if (dirPath === undefined) {
-      console.log("Must provide a directory path to statements.");
-    } else {
-      void analyzeStatements(dirPath);
-    }
+  .action(({ dir }) => {
+    void analyzeStatements(dir);
   });
 
 program.parse(process.argv);
