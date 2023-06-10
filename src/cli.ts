f2224bc81918825d0bd8acfd8c92d82d03a00b4c
Evan Gunning <45952397+evanGunning@users.noreply.github.com>
2023-06-10 10:36:12 -0700

    Add commander (#3)
    * introduce cli.ts file and npm run scripts
    
    * ignore dist/ when lintingS
    
    * make --dir option required

---
+++ src/cli.ts
@@ -0,0 +1,19 @@
+import { program } from "commander";
+import { analyzeStatements } from "./analyzers/analyzeStatements";
+
+program
+  .requiredOption(
+    "-d, --dir <dir>, Specify the directory where your statements are located"
+  )
+  .action((options) => {
+    const dirPath = options.dir;
+
+    console.log(options);
+    if (dirPath === undefined) {
+      console.log("Must provide a directory path to statements.");
+    } else {
+      void analyzeStatements(dirPath);
+    }
+  });
+
+program.parse(process.argv);
